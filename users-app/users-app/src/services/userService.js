6eb1c3c86d26954064909c8149cac4d2e42fc626
Edgar Mejia <you@example.com>
2025-02-02 20:53:22 -0600

    redux front

--- users-app/users-app/src/services/userService.js
+++ users-app/users-app/src/services/userService.js
@@ -1,34 +1,25 @@
-import axios from "axios"
+import usersApi from "../apis/usersApi";
 
-const BASE_URL = 'http://localhost:8080/users';
-
-const config = () => {
-    return {
-        headers: {
-            "Authorization": sessionStorage.getItem('token'),
-            "Content-Type": "application/json",
-        }
-    }
-}
+const BASE_URL = '';
 
 export const findAll = async() => {
     try {
-        const response = await axios.get(BASE_URL);
+        const response = await usersApi.get(BASE_URL);
         return response;
     } catch (error) {
         console.error(error);
+        throw error;
     }
-    return null;
 }
 
 export const save = async ({ username, email, password, admin }) => {
     try {
-        return await axios.post(BASE_URL, {
+        return await usersApi.post(BASE_URL, {
             username,
             email,
             password,
             admin,
-        }, config());
+        });
     } catch (error) {
         throw error;
     }
@@ -36,11 +27,11 @@ export const save = async ({ username, email, password, admin }) => {
 
 export const update = async({ id, username, email, admin }) => {
     try {
-        return await axios.put(`${BASE_URL}/${id}`, {
+        return await usersApi.put(`${BASE_URL}/${id}`, {
             username,
             email,
             admin,
-        }, config());
+        });
     } catch (error) {
         throw error;
     }
@@ -48,7 +39,7 @@ export const update = async({ id, username, email, admin }) => {
 
 export const remove = async (id) => {
     try {
-        await axios.delete(`${BASE_URL}/${id}`, config());
+        await usersApi.delete(`${BASE_URL}/${id}`);
     } catch (error) {
         throw error;
     }
